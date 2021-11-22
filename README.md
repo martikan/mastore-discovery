16c879c3739f9eed84c022980b2e848479b137c5
martikan <ric.martikan@gmail.com>
2021-11-22 19:26:08 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Spring Netflix Eureka Discovery service for MA-STORE project.
---
+++ src/test/java/org/martikan/mastore/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.martikan.mastore.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
